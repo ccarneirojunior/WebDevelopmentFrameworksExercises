5579f84cbb957c287057f50aca81ba228dbe8b45
Francisco Carneiro <69801646+ccarneirojunior@users.noreply.github.com>
2021-10-01 17:25:34 +0300

    Update Title.js

--- Exercise2/src/components/Title.js
+++ Exercise2/src/components/Title.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from './Title.module.css';
 import './Title.css';
-/* A component for rendering Title, notice how this is just a function which returns a react element */
+
 const Title = props => {
   return <div className="test">
     <h1 className={styles.title}>{ props.applicationName }</h1>
